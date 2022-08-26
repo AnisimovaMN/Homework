00acaf19ac259e96a103c6dd9775f61d98a2435f
Marina <dgoun16@mail.ru>
2022-08-26 23:30:23 +0300

    Новое решение задачи 13

--- Seminar 2/Program.cs
+++ Seminar 2/Program.cs
@@ -4,10 +4,14 @@ namespace Seminar2
     class Program
     {
         static void Main(string[] args)
-
+        
         // Задача 10. Напишите программу, которая принимает на вход трехзначное число и на выходе
         // показывает вторую цифру этого числа.
-
+        //{
+            //SecondNumber();
+        //}
+        //static void SecondNumber()
+        
         //{
             //Console.Write("Введите трехзначное число: ");
             //int number = Convert.ToInt32(Console.ReadLine());
@@ -17,47 +21,40 @@ namespace Seminar2
             
             //Console.WriteLine($"Вторая цифра числа {number} = {number2} ");
         //}
-
+        
         // Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
         // что третьей цифры нет.
 
         //{
-            //Console.Write("Введите любое число до 10000: ");
+            //ThirdNumber();
+        //}
+
+        //static void ThirdNumber()
+        //{
+            //Console.Write("Введите число: ");
             //int number = Convert.ToInt32(Console.ReadLine());
             
-            //if(number>10000)
+            //if(number > 99)
             //{
-                //Console.WriteLine("Bad number");
+                //int length = (int)Math.Log10(number);
+                //int numberPow = (int)Math.Pow(10, length - 2);
+                //int number3 = number / numberPow % 10;
+                    
+                //Console.WriteLine($"Третья цифра числа {number} = {number3} ");
             //}
-            //else 
+            //else
             //{
-                //if(number<100)
-                //{
-                    //Console.WriteLine($"Третьей цифры у числа {number} нет");
-                //}
-                //else
-                //{
-                    //if(number<1000)
-                    //{
-                        //int number1 = number % 10;
-                
-                        //Console.WriteLine($"Третья цифра числа {number} = {number1} ");
-                    //}
-                    //else
-                    //{
-                        //int number1 = number / 10;
-                        //int number2 = number1 % 10;
-
-                        //Console.WriteLine($"Третья цифра числа {number} = {number2} ");
-                    //}
-                //}
+                //Console.WriteLine($"Третьей цифры у числа {number} нет");
             //}
         //}              
-        
-
         // Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую
-        // день недели, и проверяет, является ли этот день выходным 
+        // день недели, и проверяет, является ли этот день выходным.
+           
+        //{
+            //Weekday(); 
+        //}
 
+        //static void Weekday()
         //{
             //Console.Write("Введите число дня недели от 1 до 7: ");
             //int number = Convert.ToInt32(Console.ReadLine());
@@ -79,24 +76,27 @@ namespace Seminar2
                 //}
             //}
         //}
-
         // Задача. Напишите программу, которая принимает на вход число и проверяет, 
         // кратно ли оно одновременно 7 и 23.
-
-        {
-            Console.Write("Введите число: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+        //{
+            //DivNumber();
+        //}
+        //static void DivNumber()
+        //{
+            //Console.Write("Введите число: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
             
                         
-            if(number % 7 == 0 && number % 23 == 0)
-            {
-                Console.WriteLine("да");
-            }
-            else
-            {
-                Console.WriteLine("нет");
-            }
-                
-        }
+            //if(number % 7 == 0 && number % 23 == 0)
+            //{
+                //Console.WriteLine("да");
+            //}
+            //else
+            //{
+                //Console.WriteLine("нет");
+            //}
+            
+        //}
+         
     }    
 }
